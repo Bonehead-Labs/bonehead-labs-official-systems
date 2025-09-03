5136d733280a70b9a704384f4542f9664b0bc4db
George Nizor <gnizorid@gmail.com>
2025-09-03 23:23:27 +1000

    Created base EventBus functions - GPT 5 guided
    sub, unsub, pub

--- EventBus/EventBus.gd
+++ EventBus/EventBus.gd
@@ -1,10 +1,36 @@
 class_name EventBus
 
-func pub(topic: StringName, payload: Dictionary) -> void:
-    pass
+var _subs: Dictionary = {} #Dictionary[StringName, Array[Callable]]
+
+
 
 func sub(topic: StringName, cb: Callable) -> void:
-    pass
+    var key = StringName(topic)
+    if not _subs.has(key):
+        _subs[key] = []
+    _subs[key].append(cb)
 
+
+func unsub(topic: StringName, cb: Callable) -> void:
+    var key := StringName(topic)
+    if not _subs.has(key):
+        return
+    _subs[key].erase(cb)
+    if _subs[key].is_empty():
+        _subs.erase(key)
+
+
+func pub(topic: StringName, payload: Dictionary) -> void:
+    var key := StringName(topic)
+    if not _subs.has(key):
+        return
+    var listeners: Array = _subs[key].duplicate()
+    for cb in listeners:
+        if not cb or not cb.is_valid():
+            continue
+        var err := OK
+        err = cb.call(payload)
+        if typeof(err) == TYPE_INT and err != OK:
+            print("EventBus: handler returned error on topic: ", key, " err: ", err)
 
 
